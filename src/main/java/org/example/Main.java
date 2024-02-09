42ff2c914e834f5a82a38c5d168b124ff63309be
Jesusfc <jfcaraballo@gmail.com>
2024-02-09 16:38:28 +0100

    Start Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * Author Jesús Fdez. Caraballo
+ * Created on feb - 2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
