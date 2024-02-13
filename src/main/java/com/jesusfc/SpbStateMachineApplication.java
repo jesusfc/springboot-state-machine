27ab0738eb62d0368d75c97fb1eb878c63df9cd7
Jesusfc <jfcaraballo@gmail.com>
2024-02-13 16:37:02 +0100

    Move to Maven Project

---
+++ src/main/java/com/jesusfc/SpbStateMachineApplication.java
@@ -0,0 +1,15 @@
+package com.jesusfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author Jesús Fdez. Caraballo
+ * Created on feb - 2024
+ */
+@SpringBootApplication
+public class SpbStateMachineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpbStateMachineApplication.class, args);
+    }
+}
